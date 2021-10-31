94e741a66f529a8c26aaf42102c4d465bf963993
RowilMoran <rowilmoran@gmail.com>
2021-10-31 20:52:15 -0300

    English club landing page

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,6 @@
+
+/* Accordion Script */
+
 let accordion = document.getElementsByClassName("accordion-btn");
 let i;
 
@@ -12,4 +15,44 @@ for (let i = 0; i < accordion.length; i++) {
           }
     });
 }
+
+/* Whatsapp button script */
+
+const whatsapp = document.getElementById("whatsapp");
+
+addEventListener("DOMContentLoaded", (e) => {
+  setTimeout(() => {
+    whatsapp.classList.add("appear")
+  }, 3000);
+})
+
+
+/* Fade-in and slide in Effects */
+
+const $faders = document.querySelectorAll(".fade-in");
+const $slides = document.querySelectorAll(".slide-in");
+const appearOptions = {
+  threshold: 0.10,
+  rootMargin: "0px 0px -250px 0px" 
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return 
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+$faders.forEach(fader => {
+  appearOnScroll.observe(fader)
+});
+
+$slides.forEach(slide => {
+  appearOnScroll.observe(slide)
+});
+
 
